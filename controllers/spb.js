cfae683f1ba27f4935c67fd2803221d1837c1ee3
Eric <eriyu@paypal.com>
2022-05-02 16:47:09 -0700

    - Add payment specific client ids for stage environments for Boleto and Oxxo

--- controllers/spb.js
+++ controllers/spb.js
@@ -7,6 +7,7 @@ function renderButtons(req, res, next) {
     const SDK_ENV = req.body['environment'] || 'CUSTOM';
     const SDK_CURRENCY = req.body['currency'] || 'EUR';
     const SDK_BUYER_COUNTRY = req.body['buyerCountry'] || 'NL';
+    const SELECTED_PAYMENT_SCHEME = req.body['paymentscheme'] || 'ideal';
 
     // Construct SDK URL
     const SDK_HOST = {
@@ -18,17 +19,33 @@ function renderButtons(req, res, next) {
     };
     const SDK_PATH = '/sdk/js';
     const CLIENT_ID = {
-        LOCAL:   'AZCjUMsPNzueEuqm2URngrs3LmVxfMQlFD2w3H3BNdo8f4g1Nbg0DEio_WrEpCBis7KPtw2l8OLVRiTS',
-        STAGE:   'AZCjUMsPNzueEuqm2URngrs3LmVxfMQlFD2w3H3BNdo8f4g1Nbg0DEio_WrEpCBis7KPtw2l8OLVRiTS',
-        SANDBOX: 'AXx6Sso7aff9cazBQ6WZq8kxFYEaMZk6VdRPnG3oi14fmhLLggN30PNuGl55fZABaI08qR4vjbdBkZo7',
-        LIVE:    'AaXaDMFBQrXnmaEnnaDJaGze0SHvrWrtCTaKPizp6WWgbblSmMTtkYSuDTWoZ5fjN1oQfJHflrl69V2x',
-        CUSTOM:  'AZCjUMsPNzueEuqm2URngrs3LmVxfMQlFD2w3H3BNdo8f4g1Nbg0DEio_WrEpCBis7KPtw2l8OLVRiTS'
+        LOCAL: {
+            ROW: 'AZCjUMsPNzueEuqm2URngrs3LmVxfMQlFD2w3H3BNdo8f4g1Nbg0DEio_WrEpCBis7KPtw2l8OLVRiTS',
+            OXXO: 'B_AmTklS604kjzhCGju0rjjhoY-iXBS9A0mX1sl2GrJ7cZwT4F7-0EaCK3VEBUvuhnVZtSUrzdYsJEeaj8',
+            BOLETOBANCARIO: 'AWlIT0NTvtIe8FEoLoVz9N1DjFwY3SJZ8gF-Q6w4UjbwXsB6bfFlMAJUlab6AeTMfErhsTL7PRYCk88w'
+        },
+        STAGE: {
+            ROW: 'AZCjUMsPNzueEuqm2URngrs3LmVxfMQlFD2w3H3BNdo8f4g1Nbg0DEio_WrEpCBis7KPtw2l8OLVRiTS',
+            OXXO: 'B_AmTklS604kjzhCGju0rjjhoY-iXBS9A0mX1sl2GrJ7cZwT4F7-0EaCK3VEBUvuhnVZtSUrzdYsJEeaj8',
+            BOLETOBANCARIO: 'AWlIT0NTvtIe8FEoLoVz9N1DjFwY3SJZ8gF-Q6w4UjbwXsB6bfFlMAJUlab6AeTMfErhsTL7PRYCk88w'
+        },
+        SANDBOX: {
+            ROW: 'AXx6Sso7aff9cazBQ6WZq8kxFYEaMZk6VdRPnG3oi14fmhLLggN30PNuGl55fZABaI08qR4vjbdBkZo7'
+        },
+        LIVE: {
+            ROW: 'AaXaDMFBQrXnmaEnnaDJaGze0SHvrWrtCTaKPizp6WWgbblSmMTtkYSuDTWoZ5fjN1oQfJHflrl69V2x'
+        },
+        CUSTOM: {
+            ROW: 'AZCjUMsPNzueEuqm2URngrs3LmVxfMQlFD2w3H3BNdo8f4g1Nbg0DEio_WrEpCBis7KPtw2l8OLVRiTS',
+            OXXO: 'B_AmTklS604kjzhCGju0rjjhoY-iXBS9A0mX1sl2GrJ7cZwT4F7-0EaCK3VEBUvuhnVZtSUrzdYsJEeaj8',
+            BOLETOBANCARIO: 'AWlIT0NTvtIe8FEoLoVz9N1DjFwY3SJZ8gF-Q6w4UjbwXsB6bfFlMAJUlab6AeTMfErhsTL7PRYCk88w'
+        }
     };
 
     const BRANDED_SDK_URL = util.format('%s%s?client-id=%s%s%s%s&debug=true',
         SDK_HOST[SDK_ENV],
         SDK_PATH,
-        CLIENT_ID[SDK_ENV],
+        CLIENT_ID[SDK_ENV][SELECTED_PAYMENT_SCHEME.toUpperCase()] ? CLIENT_ID[SDK_ENV][SELECTED_PAYMENT_SCHEME.toUpperCase()] : CLIENT_ID[SDK_ENV]['ROW'],
         '&components=buttons,funding-eligibility',
         util.format('&currency=%s', SDK_CURRENCY),
         util.format('&buyer-country=%s', SDK_BUYER_COUNTRY));
@@ -36,7 +53,7 @@ function renderButtons(req, res, next) {
     const UNBRANDED_SDK_URL = util.format('%s%s?client-id=%s%s%s%s&debug=true',
         SDK_HOST[SDK_ENV],
         SDK_PATH,
-        CLIENT_ID[SDK_ENV],
+        CLIENT_ID[SDK_ENV][SELECTED_PAYMENT_SCHEME.toUpperCase()] ? CLIENT_ID[SDK_ENV][SELECTED_PAYMENT_SCHEME.toUpperCase()] : CLIENT_ID[SDK_ENV]['ROW'],
         '&components=buttons,payment-fields,marks,funding-eligibility',
         util.format('&currency=%s', SDK_CURRENCY),
         util.format('&buyer-country=%s', SDK_BUYER_COUNTRY));
@@ -51,7 +68,7 @@ function renderButtons(req, res, next) {
 	const prefillValue = {
 	    environment: SDK_ENV,
 	    host: SDK_HOST[SDK_ENV],
-		paymentscheme: req.body['paymentscheme'] || 'ideal',
+		paymentscheme: SELECTED_PAYMENT_SCHEME,
 		currency: SDK_CURRENCY,
 		buyerCountry: SDK_BUYER_COUNTRY,
         buttonColor: BUTTON_STYLE.COLOR,
